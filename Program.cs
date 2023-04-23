493d88a5a3eb7391abdb6e8802a0ae33bc641220
4elovel <marcynakvova007@gmail.com>
2023-04-23 18:37:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> alphabet = new Dictionary<string, int>()
+            {
+                {" ",0},{"а",0},{"б",0},{"в",0},{"г",0},{"ґ",0},{"д",0},{"е",0},{"є",0},{"ж",0},{"з",0},{"и",0},{"і",0},{"ї",0},{"й",0},{"к",0},{"л",0},{"м",0},{"н",0},
+                {"о",0},{"п",0},{"р",0},{"с",0},{"т",0},{"у",0},{"ф",0},{"х",0},{"ц",0},{"ч",0},{"ш",0},{"щ",0},{"ь",0},{"ю",0},{"я",0}
+            };
+            string path = "..\\..\\text.txt";
+            FileStream fs = File.OpenRead(path);
+            string buf = new StreamReader(fs).ReadToEnd().ToLower();
+            fs.Close();
+            foreach (char i in buf)
+            {
+                if(Convert.ToString(i) == "\n")
+                {
+                    continue;
+                }
+                if (alphabet.ContainsKey(Convert.ToString(i)))
+                {
+                    alphabet[Convert.ToString(i)] += 1;
+                }
+                else
+                {
+                    alphabet.Add(Convert.ToString(i),1);
+                }
+            }
+            Console.OutputEncoding = Encoding.UTF8;
+            foreach (var i in alphabet)
+            {
+                Console.WriteLine($"елемент {i.Key} зустрічається {i.Value} разів");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
